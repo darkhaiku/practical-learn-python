721659d49d67ba99170cf3b095f8b66833431eff
darkhaiku <pooya.gh.alamdari@hotmail.com>
2022-10-31 21:35:46 +0330

    classmethod

---
+++ learnpython.py
@@ -0,0 +1,24 @@
+class Car:
+    runs = True
+    number_of_wheels = 4
+
+    @classmethod
+    def get_number_of_wheels(cls):
+        return cls.number_of_wheels
+
+        
+
+    def start(self):
+        if self.runs:
+            print('Car is started.Vroom!Vroom!')
+        else:
+            print("Car is broken!")
+
+my_car = Car()
+print(my_car.number_of_wheels)
+print(my_car.get_number_of_wheels())
+
+my_car.number_of_wheels = 6
+print(my_car.number_of_wheels)
+print(my_car.get_number_of_wheels())
+
