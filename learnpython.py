cae96ab8d670e22cd724bae52ca3c97063807e45
darkhaiku <pooya.gh.alamdari@hotmail.com>
2022-10-31 21:45:13 +0330

    class method output

--- learnpython.py
+++ learnpython.py
@@ -1,3 +1,7 @@
+import sys
+sys.stdout = open('practical-learn-python', 'w')
+
+
 class Car:
     runs = True
     number_of_wheels = 4
@@ -21,4 +25,6 @@ print(my_car.get_number_of_wheels())
 my_car.number_of_wheels = 6
 print(my_car.number_of_wheels)
 print(my_car.get_number_of_wheels())
+
+sys.stdout.close()
 
---
+++ test.py
@@ -0,0 +1,7 @@
+import sys
+sys.stdout = open('practical-learn-python', 'w')
+
+
+print('hello')
+
+sys.stdout.close()
