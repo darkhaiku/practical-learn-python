9c61722cf920e7554554dd1d5a46d86fe46c9079
darkhaiku <pooya.gh.alamdari@hotmail.com>
2022-11-01 13:49:53 +0330

    str and repr methods

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 import sys
 sys.stdout = open('practical-learn-python', 'w')
 
+import datetime
+now = datetime.datetime.now()
 
-print('hello')
+#that str() has returned a human-readable date/time
+print(str(now))
+print('------')
+
+#and repr() has returned a string that represents the Python code we would need to run to recreate this object.
+print(repr(now))
 
 sys.stdout.close()
